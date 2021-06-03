f86a5f69939aab7d14e5d8d125c8bcaaa3ecc408
Leonardo Coelho <lgcoelho@inf.ufpel.edu.br>
2021-06-03 13:41:20 -0300

    Get others users info

--- src/user/profile.controller.ts
+++ src/user/profile.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { use } from 'passport';
+import { UserService } from './user.service';
 
-@Controller('profile')
-export class ProfileController {}
+@Controller('profiles')
+export class ProfileController {
+    constructor(private userService:UserService){}
+
+    @Get('/:username')
+    async findProfile(@Param('username') username:string){
+        const user = await this.userService.findByUsername(username);
+        if(!user)
+            throw new NotFoundException("Usuario não encontrado");
+        
+        
+        return { profile: user };
+    }
+
+}
