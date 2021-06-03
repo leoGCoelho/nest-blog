0cf83ff059e23687ca0a515af64a58e3591b8953
Leonardo Coelho <lgcoelho@inf.ufpel.edu.br>
2021-06-03 10:36:02 -0300

    Simple user validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseConectionsService } from './database-connection.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forRootAsync({
     useClass: DatabaseConectionsService
-  })],
+  }), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { RegisterDTO, LoginDTO } from 'src/models/user.dto';
+
+@Injectable()
+export class AuthService {
+    private mockUser = {
+        email: "jake@jake.jake",
+        token: "jwt.token.here",
+        username: "jake",
+        bio: "I work at statefarm",
+        image: null
+    }
+    
+    register(credentials: RegisterDTO){
+        return this.mockUser;
+    }
+
+    login(credentials: LoginDTO) {
+        if(credentials.email === this.mockUser.email){
+            return this.mockUser;
+        }
+
+        throw new InternalServerErrorException();
+    }
+}
